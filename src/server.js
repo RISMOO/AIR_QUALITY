de647e441b78edb000459b444db0dd9605572ed1
RISMOO <maurice.sider29@gmail.com>
2020-09-28 13:59:24 +0200

    initial commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+/*
+
+const express = require ('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+app.use('/',serveStatic(path.join(__dirname,'/dist')))
+
+const port = process.env.PORT || 8000
+app.listen(port)
+
+console.log('Listenning on port:' + port)
+
+*/
